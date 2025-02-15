a12c1604d9cf71a6187971a33d3bbeab4f449700
Luciano M <132591575+luciano665@users.noreply.github.com>
2025-02-14 20:24:11 -0500

    start building the UI of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptArea from "@/components/ui/PromptArea";
+import ResponseArea from "@/components/ui/ResponseArea";
+import SubmitButton from "@/components/ui/SubmitButton";
+import callDeepSeek from "./actions/deepseek";
+import callGroq from "./actions/groq";
+import callOpenAi from "./actions/openai";
+import callGemini from "./actions/gemini";
+//
 
 export default function Home() {
+  /**
+   * Prompt area, will be using three of them
+   *
+   * 1. systemPrompt -> Optional
+   * 2. TestPrompt (userPrompt)
+   * 3. Expected asnwer
+   */
+  const [systemPrompt, setSystemPrompt] = useState("");
+  const [testQuestion, setTestQuestion] = useState("");
+  const [expectedAnswer, setExpectedAnswer] = useState("");
+
   return (
     <div className="min-h-screen bg-[#000314] bg-gradient-to-b from-[#000314] to-[#000520] p-4 sm:p-8">
       <main className="max-w-7xl mx-auto">
